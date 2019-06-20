4c7dfe36c87a5f8d1ad4915bd1a0bed4a6322543
Алена Иксанова <aaiksanova@edu.hse.ru>
2019-06-20 17:01:15 +0500

    Добавьте файлы проекта.

---
+++ Task9/MyList.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9
+{
+    class MyList
+    {
+        private Node beg;
+        private Node tail;
+        static Random rnd = new Random();
+
+        public static int Count
+        {
+            get; private set;
+        }
+
+        //добавление элементов в список
+        public Node Add(int el)
+        {
+            if (beg != null)
+            {
+                Node temp = new Node(el);
+                Node t = beg;
+                while (t.next != null) t = t.next;
+                t.next = temp;
+                temp.last = t;
+            }
+            else beg = new Node(el);
+            Count++;
+            return beg;
+        }
+
+        //создание списка с заданным количеством элементов
+        public Node Create(int n, int i)
+        {
+            if (i <= n)
+            {
+                Node head = beg;
+                head = Add(rnd.Next(-100, 100));
+                i++;
+                head = Create(n, i++);
+                return beg;
+            }
+            else return beg;
+        }
+
+        //рекурсивный метод подсчета количества элементов в списке
+        public void Count1()
+        {
+            int count = 1;
+            Node head = beg;
+            this.Counter1(count, head);
+        }
+
+        void Counter1(int count, Node head)
+        {
+            if (head != null)
+            {
+                Node temp = head;
+                if (temp.next == null)
+                {
+                    Console.WriteLine("Количество элементов в списке (рекурсивный метод): " + count);
+                }
+                else
+                {
+                    count++;
+                    temp = temp.next;
+                    Counter1(count, temp);
+                }                
+            }
+        }
+
+        //нерекурсивный метод подсчета количества элементов в списке
+        public void Count2()
+        {
+            int count = 0;
+            Node head = beg;
+            Node temp = head;
+            while (temp != null)
+            {
+                count++;
+                temp = temp.next;
+            }
+            Console.WriteLine("Количество элементов в списке: " + count);
+        }
+        
+        //печать списка
+        public void Show()
+        {
+            if (beg != null)
+            {
+                Node temp = beg;
+                Console.WriteLine("Список:");
+                while (temp != null)
+                {
+                    Console.Write(temp + " ");
+                    temp = temp.next;
+                }
+                Console.WriteLine();
+            }
+            else Console.WriteLine("Список пуст");
+        }
+    }
+
+    public class Node
+    {
+        public int data;
+        public Node next, last;
+
+        public Node()
+        {
+            data = 0;
+            next = null;
+            last = null;
+        }
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+            last = null;
+        }
+
+        public override string ToString()
+        {
+            return data.ToString() + " ";
+        }
+    }
+}
---
+++ Task9/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            bool end = false;
+            do
+            {
+                int n = rnd.Next(0, 20);
+                Console.WriteLine("Количество элементов в списке: " + n);
+                MyList l1 = new MyList();
+                l1.Create(n, 1);
+                l1.Show();
+                l1.Count1();
+                l1.Count2();
+                end = CheckKey();
+            } while (!end);
+        }
+
+        //выход из программы или формирование новой последоватльности
+        public static bool CheckKey()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            bool next, end = false;
+            int keyNum;
+            Console.WriteLine("Для выхода из программы нажмите Esc, для генерации другого списка нажмите Enter.");
+            do
+            {
+                keyNum = Console.ReadKey().KeyChar;
+                next = (keyNum == 27) || (keyNum == 13);
+            } while (!next);
+            if (keyNum == 27) end = true;
+            Console.Clear();
+            Console.ResetColor();
+            return end;
+        }
+    }
+}
